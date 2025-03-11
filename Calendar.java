aaad86be771e03d9b570a789a5769a0059dfd1d2
anush-matevosyan <j8d4n@unb.ca>
2025-03-11 00:29:08 -0300

    adding the input and calendar java files

---
+++ Calendar.java
@@ -0,0 +1,20 @@
+public class Calendar{
+	int date;
+	int month;
+	int year;
+	
+	public Calendar(int yearIn){
+		year = //need to figure out the format the excel stores stuff
+	}
+	
+	public Calendar(int yearIn, int monthIn){
+		year = //need to figure out the format the excel stores stuff
+		month = monthIn;//again 
+	}
+	
+	public Calendar(int yearIn, int monthIn, int dayIn){
+		year = //need to figure out the format the excel stores stuff
+		month = monthIn;//again 
+	}
+	
+}
---
+++ Input.java
@@ -0,0 +1,16 @@
+import Date;
+
+public class Input{
+	String category;
+	double amount;
+	String notes;
+	Calendar date;
+	
+	public Input(String catIn, double amountIn, String notesIn, Calendar dateIn){
+		category = catIn;
+		amount = amountIn;
+		notes = notesIn;
+		date = dateIn; //figure out how date will be stores
+	}
+	
+}
