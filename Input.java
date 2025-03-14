68382a8a0e17273616fb8f8d7c45f12bc9f4f1fe
anush-matevosyan <j8d4n@unb.ca>
2025-03-14 09:13:50 -0300

    Update Input.java

--- Input.java
+++ Input.java
@@ -8,7 +8,20 @@ public class Input{
 		category = catIn;
 		amount = amountIn;
 		notes = notesIn;
-		date = dateIn; 
+		date = dateIn;
 	}
 	
+	public Input(String catIn, double amountIn, Calendar dateIn){
+		category = catIn;
+		amount = amountIn;
+		notes = null;
+		date = dateIn; //figure out how date will be stores
+	}
+	
+	public String toString(){
+		if(notes != null){
+			return "Category: " + category + " Amount: $" + amount +"Notes: " + notes + " Date: " + date;
+		}
+		return "Category: " + category + " Amount: $" + amount + " Date: " + date;
+	}	
 }
