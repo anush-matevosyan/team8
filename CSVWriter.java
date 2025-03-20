d59bc4ffbe66acf29c343a2c8870db37b404da64
anush-matevosyan <j8d4n@unb.ca>
2025-03-19 21:37:43 -0300

    Update CSVWriter.java

--- CSVWriter.java
+++ CSVWriter.java
@@ -1,103 +1,49 @@
-import java.io.File;
-import java.io.FileOutputStream; 
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
-import org.apache.poi.ss.usermodel.Cell; 
-import org.apache.poi.ss.usermodel.Row; 
-import org.apache.poi.xssf.usermodel.XSSFSheet; 
-import org.apache.poi.xssf.usermodel.XSSFWorkbook; 
-
-public class CSVWriter {
-
-	public static void main(String[] args) {
-		XSSFWorkbook workbook = new XSSFWorkbook();
-		
-		XSSFSheet spendings = workbook.createSheet("spendings");
-		
-		XSSFSheet income = workbook.createSheet("income");
-		
-		Map<String, Object[]> dataSpend = new TreeMap<String, Object[]>(); 
-		
-		Map<String, Object[]> dataIncome = new TreeMap<String, Object[]>(); 
-		
-		dataSpend.put("1", new Object[] {"Date", "Amount", "Category", "Notes"});
-		//get the data from the GUI, hard coded for now
-		dataSpend.put("2", new Object[] {"12/15/2024", "$20", "Food", "Treat"});
-		dataSpend.put("3", new Object[] {"03/12/2025", "$500", "Bills", "Rent"});
-		dataSpend.put("4", new Object[] {"03/13/2025", "$200", "Food", "Groceries"});
-		
-		dataIncome.put("1", new Object[] {"Date", "Amount Earned"});
-		dataIncome.put("2", new Object[] {"02/28/2025", "$190"});
-		
-		Set<String> keyset = dataSpend.keySet();
-		
-		Set<String> keyset2 = dataIncome.keySet();
-		
-		int rownum = 0;
-		
-		ArrayList<String> keyList = new ArrayList<>(dataSpend.keySet());
-		
-		ArrayList<String> keyListIncome = new ArrayList<>(dataIncome.keySet());
-		
-		//writing into spendings
-		for (int i = 0; i < keyList.size(); i++) {
-			String key = keyList.get(i);
-			Row row = spendings.createRow(i);
-
-			Object[] objArr = dataSpend.get(key);
-			int cellnum = 0;
-
-			for (int j = 0; j < objArr.length; j++) {
-				Cell cell = row.createCell(cellnum++);
-				cell.setCellValue((String) objArr[j]);
-			}
-		}
-		//writing into income
-		for (int i = 0; i < keyListIncome.size(); i++) {
-			String key = keyListIncome.get(i);
-			Row row = income.createRow(i);  
-
-			Object[] objArr = dataIncome.get(key);
-			int cellnum = 0;
-
-			for (int j = 0; j < objArr.length; j++) {
-				Cell cell = row.createCell(cellnum++);
-				cell.setCellValue((String) objArr[j]);
-			}
-		}
-		
+public class Writing{
+	private String fileName;
+	
+	public Writing(String fileNameIn){
+		fileName = fileNameIn;
+	}
+	
+	public void addData(String date, String amount, String category, String notes){
+		boolean exists = true; 
 		try{
-			/**
-			from the gui ask the user if they have used the application before.
-			if yes then open an existing workbook
-			if no, create a new one 
-			**/			
-			boolean exists = true; //info from gui, hard coded for now
-			String name = "Tom";
-			String lastName = "Felton";		
-			String fileName = name+lastName+".xlsx";
-			FileOutputStream out;
-			if (!exists){
-				File f1 = new File(fileName);
-				out = new FileOutputStream(f1);
+			File file = new File(fileName);
+			ArrayList<String> notes = new ArrayList<>();
+			int rows = 0;
+			Scanner reader = new Scanner(file);
+			while(reader.hasNextLine()){
+				reader.nextLine();
+				rows++;
 			}
-			else{
-				out = new FileOutputStream(fileName);
+			reader.close();
+		}
+		catch(FileNotFoundException e){
+			exists = false;
+			System.out.println("here");
+		}
+		try{
+			FileWriter file;
+			file = new FileWriter(fileName, true);
+			if(!exists){
+				
+				file.append("Date, Amount, Category, Notes\n");
 			}
+		
+			file.append(date + ",");
+			file.append(amount + ",");
+			file.append(category + ",");
+			file.append(notes + ",\n");
 			
-			workbook.write(out);
-			
-			out.close();
-			
-			System.out.println("\n" + fileName + " written successfully");
-			
+			file.close();
 		}
-		catch (Exception e){
-			e.printStackTrace();
+		catch(IOException e){
+			System.out.println(e);
 		}
+		
+		
 	}
 }
