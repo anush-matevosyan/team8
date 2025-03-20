623c64ad7dc71b166c58ea105e6495e15f263826
anush-matevosyan <j8d4n@unb.ca>
2025-03-19 23:48:18 -0300

    Update SetSavingsGoal.java

---
+++ SetSavingsGoal.java
@@ -0,0 +1,62 @@
+import javafx.geometry.Pos; 
+
+import javafx.scene.Scene; 
+
+import javafx.scene.control.*; 
+
+import javafx.scene.layout.VBox; 
+
+import javafx.stage.Modality; 
+
+import javafx.stage.Stage; 
+
+import java.io.IOException; 
+
+import java.nio.file.Files; 
+
+import java.nio.file.Paths; 
+
+import java.util.List; 
+
+import javafx.scene.layout.*;
+
+import javafx.geometry.Insets;
+
+public class SetSavingsGoal {
+
+    public SetSavingsGoal(Stage parentStage) {
+        
+		Stage savingsStage = new Stage();
+		
+		savingsStage.initModality(Modality.APPLICATION_MODAL); 
+
+        savingsStage.setTitle("Set Savings Goal");
+
+        Label lblGoal = new Label("Enter your monthly savings goal:");
+        TextField txtGoal = new TextField();
+		Writing write = new Writing("savingsGoal.txt");
+        double existingGoal = write.loadSavingsGoal();
+        if (existingGoal != -1) {
+            txtGoal.setText(String.valueOf(existingGoal));
+        }
+
+        Button btnSave = new Button("Save Goal");
+        btnSave.setOnAction(e -> {
+            try {
+                double goal = Double.parseDouble(txtGoal.getText());
+                write.saveSavingsGoal(goal);
+                savingsStage.close();
+            } catch (NumberFormatException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid goal amount!");
+                alert.showAndWait();
+            }
+        });
+
+        VBox layout = new VBox(10, lblGoal, txtGoal, btnSave);
+        layout.setPadding(new Insets(20));
+
+        Scene scene = new Scene(layout, 300, 150);
+        savingsStage.setScene(scene);
+        savingsStage.showAndWait();
+    }
+}
