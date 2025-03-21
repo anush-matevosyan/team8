0af7a68184c30e8f029af893395e9b8e84e4203c
anush-matevosyan <j8d4n@unb.ca>
2025-03-21 11:46:09 -0300

    Added TransactionHistory.java

---
+++ TransactionHistory.java
@@ -0,0 +1,51 @@
+import javafx.geometry.Pos; 
+
+import javafx.scene.Scene; 
+
+import javafx.scene.control.*; 
+
+import javafx.scene.layout.VBox; 
+
+import javafx.stage.Modality; 
+
+import javafx.stage.Stage; 
+
+import java.io.IOException; 
+
+import java.nio.file.Files; 
+
+import java.nio.file.Paths; 
+
+import java.util.List; 
+
+  
+
+public class TransactionHistory { 
+
+    public TransactionHistory(Stage parentStage) { 
+
+        Stage transactionStage = new Stage(); 
+
+        transactionStage.initModality(Modality.APPLICATION_MODAL); 
+
+        transactionStage.setTitle("Transaction History"); 
+
+		String fileName = "spendings.csv";
+		CSVReader.read(fileName);
+
+		Label history = new Label("Your text file with the transaction history \nhas been created!");
+		//fix color
+
+        VBox layout = new VBox(10, history); 
+
+        layout.setAlignment(Pos.CENTER); 
+
+        Scene scene = new Scene(layout, 300, 100); 
+
+        transactionStage.setScene(scene); 
+
+        transactionStage.showAndWait(); 
+		//close once entry is added 
+    } 
+
+}
